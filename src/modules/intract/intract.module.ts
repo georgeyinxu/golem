b6c0826845194839e1e021440fd349be6d4702bb
George Yin Xu <georgeyinxu@gmail.com>
2024-01-15 12:20:47 +0800

    completed bridging tracker

--- src/modules/intract/intract.module.ts
+++ src/modules/intract/intract.module.ts
@@ -94,6 +94,14 @@ export default async function IntractModule(
         return reply.status(400).send(response);
       }
 
+      if (!process.env.SALD_POS_ADDR) {
+        response.error = {
+          code: 400,
+          message: "Please ensure that SALD_POS_ADDR is set.",
+        };
+        return reply.status(400).send(response);
+      }
+
       if (!minAmount) {
         response.error = {
           code: 400,
@@ -107,7 +115,7 @@ export default async function IntractModule(
           params: {
             module: "account",
             action: "tokenbalance",
-            contractaddress: "0x6A34A7284B13bA70A6135bE9738Da35C5eF22f78",
+            contractaddress: process.env.SALD_POS_ADDR,
             address,
             tag: "latest",
             apikey: process.env.POLYGONSCAN_API_KEY,
@@ -117,6 +125,118 @@ export default async function IntractModule(
         const balance = parseFloat(ethers.formatEther(res.data.result));
 
         if (balance > minAmount) {
+          response.data.result = true;
+        }
+      } catch (error: any) {
+        response.error = { code: error.code, message: error.message };
+      }
+
+      return reply.send(response);
+    }
+  );
+
+  fastify.post<{ Body: IntractInterface }>(
+    "/intract/bridge",
+    intractSchema,
+    async (request, reply) => {
+      const { address } = request.body;
+      let response: {
+        error?: { code: number; message: string };
+        data: { result: boolean };
+      } = { data: { result: false } };
+      let polygonTotal = 0;
+      let ethTotal = 0;
+
+      if (!process.env.POLYGONSCAN_API_KEY) {
+        response.error = {
+          code: 400,
+          message: "Please ensure that POLYGONSCAN_API_KEY is set.",
+        };
+        return reply.status(400).send(response);
+      }
+
+      if (!process.env.ETHERSCAN_API_KEY) {
+        response.error = {
+          code: 400,
+          message: "Please ensure that ETHERSCAN_API_KEY is set.",
+        };
+        return reply.status(400).send(response);
+      }
+
+      if (!process.env.SALD_POS_ADDR) {
+        response.error = {
+          code: 400,
+          message: "Please ensure that SALD_POS_ADDR is set.",
+        };
+        return reply.status(400).send(response);
+      }
+
+      if (!process.env.SALD_ETH_ADDR) {
+        response.error = {
+          code: 400,
+          message: "Please ensure that SALD_ETH_ADDR is set.",
+        };
+        return reply.status(400).send(response);
+      }
+
+      try {
+        // Check the brdiging on Ethereum
+        const ethereumRes = await axios.get("https://api.etherscan.com/api", {
+          params: {
+            module: "account",
+            action: "tokentx",
+            address: address.toLowerCase(),
+            contractaddress: process.env.SALD_ETH_ADDR,
+            apikey: process.env.ETHERSCAN_API_KEY,
+          },
+        });
+
+        const ethTx = ethereumRes.data.result;
+
+        if (ethTx) {
+          const bridgeTransfers = ethTx.filter(
+            (tx: any) =>
+              tx.from.toLowerCase() === address.toLowerCase() &&
+              tx.tokenSymbol === "SALD" &&
+              tx.to === "0x1646a4761aa54f23d7f4d5deb5d393f67d318b80"
+          );
+
+          if (bridgeTransfers.length > 0) {
+            bridgeTransfers.map((tx: any) => {
+              ethTotal += parseFloat(ethers.formatEther(tx.value));
+            });
+          }
+        }
+
+        // Check the bridging on Polygon
+        const polygonRes = await axios.get("https://api.polygonscan.com/api", {
+          params: {
+            module: "account",
+            action: "tokentx",
+            address: address.toLowerCase(),
+            contractaddress: process.env.SALD_POS_ADDR,
+            apikey: process.env.POLYGONSCAN_API_KEY,
+          },
+        });
+
+        const polygonTx = polygonRes.data.result;
+
+        if (polygonTx) {
+          const bridgeTransfers = polygonTx.filter(
+            (tx: any) =>
+              tx.from.toLowerCase() ===
+                "0x0000000000000000000000000000000000000000" &&
+              tx.tokenSymbol === "SALD"
+          );
+
+          if (bridgeTransfers.length > 0) {
+            bridgeTransfers.map((tx: any) => {
+              polygonTotal += parseFloat(ethers.formatEther(tx.value));
+            });
+          }
+        }
+
+        if (polygonTotal === ethTotal && polygonTotal > 1000) {
           response.data.result = true;
         }
       } catch (error: any) {
