0d0070c1a160b269e5c4ecc8d08b4140733a4126
George Yin Xu <georgeyinxu@gmail.com>
2024-01-17 16:50:04 +0800

    updated prisma schema for twitter comments, likes and retweet

--- src/modules/twitter/twitter.module.ts
+++ src/modules/twitter/twitter.module.ts
@@ -17,8 +17,33 @@ export default async function TwitterModule(
     } = { result: { username: "", points: "" } };
 
     try {
-        const tweet = await twitterClient.v2.user(id);
-        console.log(tweet);
+      const tweetsResponse = await twitterClient.v2.userTimeline(id, {
+        start_time: "2024-01-01T00:00:00Z",
+        "tweet.fields": ["id", "text", "created_at"],
+      });
+      const tweetsFromSV = tweetsResponse.data.data;
+
+      // TODO: Store the tweets from SV into DB
+
+      // Getting all the replies, likes and retweets of a specific post
+      tweetsFromSV.map(async (tweet) => {
+        // const usersWhoLiked = await twitterClient.v2.tweetLikedBy(tweet.id, { asPaginator: true });
+
+        // console.log(`Users Liked: ${tweet.id}`, usersWhoLiked.data);
+        // const usersWhoRetweeted = await twitterClient.v2.tweetRetweetedBy(
+        //   tweet.id,
+        //   {
+        //     asPaginator: true,
+        //   }
+        // );
+
+        // console.log(
+        //   `Users who re-tweeted: ${tweet.id}`,
+        //   usersWhoRetweeted.data
+        // );
+
+        // TODO: How should brownie points be calculated?
+      });
     } catch (error: any) {
       response.error = { code: error.code, message: error.message };
       return reply.status(error.code).send(response);
